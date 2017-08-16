cf98082ac8411cbc7d3a386fbe34cf51901cba3d
zenghuichen <30993206+zenghuichen@users.noreply.github.com>
2017-08-17 01:54:00 +0800

    zenghuichen
    这个是我最新的程序版本

---
+++ redMomery/Controllers/LBController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace redMomery.Controllers
+{
+    public class LBController : Controller
+    {
+        //
+        // GET: /LB/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LB()
+        {
+            return View();
+        }
+    }
+}
