3fabb3f56a06b1965b773e9cf132b814b12e6821
zenghuichen <30993206+zenghuichen@users.noreply.github.com>
2017-08-17 10:30:13 +0800

    解决程序编译问题，主要是因为版本化管理导致的

--- redMomery/Controllers/LBZLController.cs
+++ redMomery/Controllers/LBZLController.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,25 +24,4 @@ namespace redMomery.Controllers
 
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 
-namespace redMomery.Controllers
-{
-    public class LBZLController : Controller
-    {
-        //
-        // GET: /LBZL/
-
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-    }
-}
->>>>>>> d80a3d1fac9b2cc38f316903fe8ecb6bda32078a
