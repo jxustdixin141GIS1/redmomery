8171a2f8806f551638307d3b3b23e8e5003dd714
zenghuichen <30993206+zenghuichen@users.noreply.github.com>
2017-08-19 12:28:31 +0800

    zenghuichen
    中午代码提交保存代码，预防电脑没电

--- redMomery/resource/staticfile/Treatmentdata.js
+++ redMomery/resource/staticfile/Treatmentdata.js
@@ -4,150 +4,97 @@ var myChart = null;//这个必须声明
 var data = null;
 var geoCoordMap = null;
 var isdealwithok = false;//这个表示前一个运算还没有结束
-function distributioncity() {
-    var dom = document.getElementById("container");
-    var myChart = echarts.init(dom);
-    var app = {};
-    option = null;
 
-    var data = [];
-    var geoCoordMap = {};
-    //下面开始进行生成
-    for (var i in staticinfo)
-    {       
-        var temp = staticinfo[i]; //temp.Key  CityCode,temp.value  统计人数;
-        var cityobject=getcitybycitycode(temp.Key);//cityobject :  城市对象
-        var datatemp = {};
-        if (i == 96)
-        {
-            console.log(i);
+//-----------------老兵性别统计-------------------------------------------------
+function GetLbsexlist()
+{
+    $.ajax({
+        url: "/Statistics/GetLbsexstatstic",
+        type: "post",
+        dataType: "json",
+        success: function (result) {
+            data = result;
+            //进行数据处理
+            var temp = [];
+            for (var i = 0; i < data.length; i++) {
+                var datatemp = {};
+                datatemp["name"] = data[i].Key;
+                datatemp["value"] = data[i].Value;
+                if(data[i])
+
+                temp.push(datatemp);
+            }
+            data = temp;
+            myChart.hideLoading();
+            //下面开始进行统计的展示计算，这里单独变成一个方法
+            loadedLbsexList();
         }
-        datatemp["name"] = cityobject.cityname;
-        datatemp["value"] = temp.value;
-        data.push(datatemp);
-        //开始针对地理进行展示
-        geoCoordMap[cityobject.cityname]=cityobject.coordination;
-    }
-
-    //结束生成
-    var convertData = function (data) {
-        var res = [];
-        for (var i = 0; i < data.length; i++) {
-            var geoCoord = geoCoordMap[data[i].name];
-            if (geoCoord) {
-                res.push({
-                    name: data[i].name,
-                    value: geoCoord.concat(data[i].value)
-                });
-            }
-        }
-        return res;
-    };
-
-    option = {
-        backgroundColor: '#404a59',
-        title: {
-            text: '全国主要城市老兵分布',
-            subtext: 'data from PM25.in',
-            sublink: 'http://www.pm25.in',
-            left: 'center',
-            textStyle: {
-                color: '#fff'
-            }
-        },
-        tooltip: {
-            trigger: 'item'
-        },
-        legend: {
-            orient: 'vertical',
-            y: 'bottom',
-            x: 'right',
-            data: ['pm2.5'],
-            textStyle: {
-                color: '#fff'
-            }
-        },
-        geo: {
-            map: 'china',
-            label: {
-                emphasis: {
-                    show: false
-                }
-            },
-            roam: true,
-            itemStyle: {
-                normal: {
-                    areaColor: '#323c48',
-                    borderColor: '#111'
-                },
-                emphasis: {
-                    areaColor: '#2a333d'
-                }
-            }
-        },
-        series: [
-            {
-                name: 'pm2.5',
-                type: 'scatter',
-                coordinateSystem: 'geo',
-                data: convertData(data),
-                symbolSize: function (val) {
-                    return val[2] / 10;
-                },
-                label: {
-                    normal: {
-                        formatter: '{b}',
-                        position: 'right',
-                        show: false
-                    },
-                    emphasis: {
-                        show: true
-                    }
-                },
-                itemStyle: {
-                    normal: {
-                        color: '#ddb926'
-                    }
-                }
-            },
-            {
-                name: 'Top 5',
-                type: 'effectScatter',
-                coordinateSystem: 'geo',
-                data: convertData(data.sort(function (a, b) {
-                    return b.value - a.value;
-                }).slice(0, 6)),
-                symbolSize: function (val) {
-                    return val[2] / 10;
-                },
-                showEffectOn: 'render',
-                rippleEffect: {
-                    brushType: 'stroke'
-                },
-                hoverAnimation: true,
-                label: {
-                    normal: {
-                        formatter: '{b}',
-                        position: 'right',
-                        show: true
-                    }
-                },
-                itemStyle: {
-                    normal: {
-                        color: '#f4e925',
-                        shadowBlur: 10,
-                        shadowColor: '#333'
-                    }
-                },
-                zlevel: 1
-            }
-        ]
-    };
-    ;
-    if (option && typeof option === "object") {
-        myChart.setOption(option, true);
-    }
+    });
 }
+function loadedLbsexList()
+{
+    var app = {};
+    var option = null;
+        option = {
+            title : {
+                text: '老兵性别比',
+                x: 'center'
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left',
+                data: ['男','女']
+            },
+            series : [
+                {
+                    name: '老兵性别',
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data:data,
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+
+    app.currentIndex = -1;
+
+    setInterval(function () {
+        var dataLen = option.series[0].data.length;
+        // 取消之前高亮的图形
+        myChart.dispatchAction({
+            type: 'downplay',
+            seriesIndex: 0,
+            dataIndex: app.currentIndex
+        });
+        app.currentIndex = (app.currentIndex + 1) % dataLen;
+        // 高亮当前图形
+        myChart.dispatchAction({
+            type: 'highlight',
+            seriesIndex: 0,
+            dataIndex: app.currentIndex
+        });
+        // 显示 tooltip
+        myChart.dispatchAction({
+            type: 'showTip',
+            seriesIndex: 0,
+            dataIndex: app.currentIndex
+        });
+    }, 1000);
+
+    myChart.setOption(option);
+}
+//------------------老兵城市分布方法集合------------------------------------------
 function loadedmyChart()
 {
     var app = {};
