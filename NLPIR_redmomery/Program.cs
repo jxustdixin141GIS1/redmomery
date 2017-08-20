3c75ee062f08ff78a83f3b6ebcec1881fdb4c085
zenghuichen <30993206+zenghuichen@users.noreply.github.com>
2017-08-20 11:39:51 +0800

    zenghuichen
    保存源代码，以防数据丢失

---
+++ NLPIR_redmomery/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLPIR_redmomery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
