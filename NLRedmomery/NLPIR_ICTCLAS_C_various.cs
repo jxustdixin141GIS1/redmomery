e06498b44c765c5b097220a68c87c3467135fbe5
zenghuichen <30993206+zenghuichen@users.noreply.github.com>
2017-08-24 13:59:22 +0800

    zenghuichen
    平台版本改为x86

--- NLRedmomery/NLPIR_ICTCLAS_C_various.cs
+++ NLRedmomery/NLPIR_ICTCLAS_C_various.cs
@@ -14,14 +14,14 @@ namespace NLRedmomery
         #region 文件初始化地址变量
         private const string NLpath = @"D:\题库系统\github\team\redmomery\NLRedmomery\bin\Debug\";
         private const string rootDir =NLpath+ @"..\..\NLPIR\";
-        private const string libpath = NLpath + @"..\..\NLPIR\bin-win64\";
+        private const string libpath = NLpath + @"..\..\NLPIR\bin-win32\";
         private const string datapath = NLpath + @"..\..\NLPIR\Data\";
         private const string sentimentData = NLpath + @"..\..\NLPIR\Data\SentimentNew";
-        private const string NLPIRPath = NLpath + @"..\..\NLPIR\bin-win64\NLPIR.dll";
-        private const string KeyExtractPath = NLpath + @"..\..\NLPIR\bin-win64\KeyExtract.dll";
-        private const string NewWordFinder = NLpath + @"..\..\NLPIR\bin-win64\NewWordFinder.dll";
-        private const string DElib = NLpath + @"..\..\NLPIR\bin-win64\DocExtractor.dll";
-        public  const string userDir = NLpath + @"..\..\NLPIR\bin-win64\\output\NewTermlist.txt";
+        private const string NLPIRPath = libpath + @"\NLPIR.dll";
+        private const string KeyExtractPath = libpath + @"\KeyExtract.dll";
+        private const string NewWordFinder = libpath + @"\NewWordFinder.dll";
+        private const string DElib = libpath + @"\DocExtractor.dll";
+        public const string userDir = libpath + @"\output\NewTermlist.txt";
         #endregion
 
     }
